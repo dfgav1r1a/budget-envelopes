7dca07a5bf7b94d448743b421079a867b6f48556
Daniel Gaviria <danielfgaviria@gmail.com>
2024-02-05 22:07:24 -0500

    implementing  GET and POST req for single envelopes

--- backend/index.js
+++ backend/index.js
@@ -6,7 +6,15 @@ const envelopes= require('./envelopesData');
 app.use(express.json());
 
 app.get('/envelopes', (req, res, next) => {
- res.send(envelopes);
+ res.send(envelopes.envelopesData.data);
+});
+
+app.get('/envelopes/envelope/:id', (req, res, next) => {
+    const id = Number(req.params.id);
+    const envelope = envelopes.envelopesData.data.find(obj => obj.id === id);
+    console.log(envelope);
+    if (!envelope) res.status(400).send('Envelope not found');
+    res.status(200).send(envelope); 
 });
 
 app.post('/envelopes', (req, res, next) => {
@@ -14,6 +22,11 @@ app.post('/envelopes', (req, res, next) => {
     envelopes.envelopesData.data.push(payload);
     console.log(envelopes);
     res.send(envelopes)
+});
+
+app.put('/envelopes/envelope/id', (req, res, next) => {
+    const id = Number(req.params.id);
+
 });
 
 app.listen(PORT, () => {

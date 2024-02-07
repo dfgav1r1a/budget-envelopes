bfc3e41e21197a75f0b7fac9073ecaf881385e8d
Daniel Gaviria <danielfgaviria@gmail.com>
2024-02-06 21:46:49 -0500

    implementing initial delete

--- backend/index.js
+++ backend/index.js
@@ -12,7 +12,6 @@ app.get('/envelopes', (req, res, next) => {
 app.get('/envelopes/envelope/:id', (req, res, next) => {
     const id = Number(req.params.id);
     const envelope = envelopes.envelopesData.data.find(obj => obj.id === id);
-    console.log(envelope);
     if (!envelope) res.status(400).send('Envelope not found');
     res.status(200).send(envelope); 
 });
@@ -20,13 +19,32 @@ app.get('/envelopes/envelope/:id', (req, res, next) => {
 app.post('/envelopes', (req, res, next) => {
     const payload = req.body;
     envelopes.envelopesData.data.push(payload);
-    console.log(envelopes);
     res.send(envelopes)
 });
 
-app.put('/envelopes/envelope/id', (req, res, next) => {
+app.put('/envelopes/envelope/:id', (req, res, next) => {
     const id = Number(req.params.id);
+    const edits = req.body;
+    const envelope = envelopes.envelopesData.data.find(obj => obj.id === id);
+    if (edits.category !== envelope.category) envelope.category = edits.category;
+    if (envelope.subcategories) {
+        for (const object in envelope.subcategories) {
+            console.log(envelope.subcategories[object]);    
+            //trying to compare values of envelopes before updating them.
+        }
+    } 
+    res.send(envelope)
+});
 
+app.delete('/envelopes/envelope/:id', (req, res, next) => {
+    const id = Number(req.params.id);
+    //finding envelope
+    const envelope = envelopes.envelopesData.data.find(obj => obj.id === id);
+    //finding the index
+    const indexToDelete = envelopes.envelopesData.data.indexOf(envelope);
+    //deleting the envelope in the data array using its index
+    envelopes.envelopesData.data.splice(indexToDelete, 1)
+    res.send('deleted')
 });
 
 app.listen(PORT, () => {
